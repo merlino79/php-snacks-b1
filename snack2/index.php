fefded3da0bbea7831bfdec09997af2e754c2a13
Merlino79 <alessandro.ciancio@hotmail.com>
2021-06-04 19:29:41 +0200

    prova get con variabili

---
+++ snack2/index.php
@@ -0,0 +1,27 @@
+<!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>snack2</title>
+</head>
+<body>
+   <?php
+    $name = $_GET['name'] ;
+    $age = $_GET['age'] ;
+    $mail = $_GET['mail'] ;
+
+     ?>
+
+  <p>
+  ciao
+  <?php echo $name . ' ' . $age . $mail?>
+  </p>
+
+  
+</body>
+</html>
